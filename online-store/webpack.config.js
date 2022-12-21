4a55b0b764defb3bc54bc902109dca4342b7f06e
Ilyas022 <ilya.shchetko@mail.ru>
2022-12-21 14:14:45 +0300

    refactor: changed config

--- online-store/webpack.config.js
+++ online-store/webpack.config.js
@@ -3,6 +3,8 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const ESLintPlugin = require('eslint-webpack-plugin')
 
+const getCSSModuleLocalIdent = require('react-dev-utils/getCSSModuleLocalIdent');
+
 const devServer = (isDev) => !isDev ? {} : {
   devServer: {
     open: true,
@@ -26,9 +28,9 @@ module.exports = ({ dev }) => ({
       {
         test: /\.(png|svg|jpg|jpeg|gif|ico)$/i,
         type: 'asset/resource',
-        generator: {
-          filename: './assets/a/[hash][ext]'
-        }
+        // generator: {
+        //   filename: './assets/a/[hash][ext]'
+        // }
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
@@ -45,6 +47,22 @@ module.exports = ({ dev }) => ({
           MiniCssExtractPlugin.loader,
           "css-loader",
         ],
+        exclude: /\.module\.css$/
+      },
+      {
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              modules: {
+                mode: "local",
+                getLocalIdent: getCSSModuleLocalIdent,
+              },
+            }
+          }
+        ],
+        include: /\.module\.css$/
       },
       {
         test: /\.s[ac]ss$/i,
@@ -53,6 +71,23 @@ module.exports = ({ dev }) => ({
           "css-loader",
           "sass-loader",
         ],
+        exclude: /\.module\.scss$/
+      },
+      {
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              modules: {
+                mode: "local",
+                getLocalIdent: getCSSModuleLocalIdent,
+              },
+            }
+          },
+          "sass-loader",
+        ],
+        include: /\.module\.scss$/
       },
     ]
   },
@@ -64,7 +99,7 @@ module.exports = ({ dev }) => ({
       template: './src/index.html'
     }),
     new MiniCssExtractPlugin({
-      filename: '[contenthash].css'
+      filename: dev ? '[name].css' : '[name].[contenthash].css'
     }),
     new ESLintPlugin({
       extensions: 'ts'
