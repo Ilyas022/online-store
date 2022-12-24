5363fa80d1676e239c16bdd217db88b06f820132
Ilyas022 <ilya.shchetko@mail.ru>
2022-12-24 15:03:00 +0300

    feat: add footer

--- online-store/src/App.tsx
+++ online-store/src/App.tsx
@@ -1,13 +1,14 @@
 import React from "react";
+import Footer from "./components/footer/footer";
 import Header from "./components/header/header";
 import MainPage from "./components/pages/mainPage/mainPage";
-
 
 const App = () => {
   return (
   <>
     <Header />
     <MainPage />
+    <Footer />
   </>
   )
 }
--- online-store/src/components/logo.tsx
+++ online-store/src/components/logo.tsx
@@ -3,7 +3,7 @@ import logo from '../assets/img/icons/logo.svg'
 
 export default function headerLogo() {
   return (
-    <a className="header__logo logo" href="#">
+    <a className="logo" href="#">
       <img className="logo__img" src={logo} alt="" />
       <div className="logo__text">FIRST TEA COMPANY</div>
   </a>
